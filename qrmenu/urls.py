0e9d8f3ecd922568175b7baca1a76977060640da
almircoo <almircoo@proton.me>
2025-10-03 21:38:27 +0100

    registered views for urls

--- qrmenu/urls.py
+++ qrmenu/urls.py
@@ -7,6 +7,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from core import views
 schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
@@ -21,12 +22,15 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+   path('admin/', admin.site.urls),
 
-    # path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+   path('auth/', include('djoser.urls')),
+   path('auth/', include('djoser.urls.authtoken')),
+
+   # path('swagger.<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
+   # core views
+   path('api/places/', views.PlaceList.as_view()),
+   path('api/places/<pk>', views.PlaceDetail.as_view()),
 ]
