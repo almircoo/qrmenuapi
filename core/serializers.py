a1dcaceed27d53ae6e33b2d570940f0134a23179
almircoo <almircoo@proton.me>
2025-10-08 18:55:14 +0100

    serialized category and menuitems

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 from . import models
 
+class MenuItemSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = models.MenuItem
+    fields = ('id', 'name', 'description', 'price', 'image', 'is_available', 'place', 'category')
+
+class CategorySerializer(serializers.ModelSerializer):
+  menu_items = MenuItemSerializer(many=True, read_only=True)
+
+  class Meta:
+    model = models.Category
+    fields = ('id', 'name', 'menu_items', 'place')
 
 class PlaceDetailSerializer(serializers.ModelSerializer):
   class Meta:
