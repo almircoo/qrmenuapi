59db292cf1aa0388ece3a1f5d1cbc476fe97bb5a
almircoo <almircoo@proton.me>
2025-10-08 18:58:02 +0100

    create order model and migrated

--- core/models.py
+++ core/models.py
@@ -33,3 +33,22 @@ class MenuItem(models.Model):
 
   def __str__(self):
     return "{}/{}".format(self.category, self.name)
+  
+class Order(models.Model):
+  PROCESSING_STATUS = "processing"
+  COMPLETED_STATUS = "completed"
+  STATUSES = (
+    (PROCESSING_STATUS, 'Processing'),
+    (COMPLETED_STATUS, 'Completed'),
+  )
+
+  place = models.ForeignKey(Place, on_delete=models.CASCADE)
+  table = models.CharField(max_length=2)
+  detail = models.TextField()
+  payment_intent = models.CharField(max_length=255)
+  amount = models.IntegerField()
+  status = models.CharField(max_length=20, choices=STATUSES, default=PROCESSING_STATUS)
+  created_at = models.DateTimeField(default=timezone.now)
+
+  def __str__(self):
+    return "{}/{}/${}".format(self.place, self.table, self.amount)
