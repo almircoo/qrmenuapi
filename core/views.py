417d84dbc78962d6a30a766dcb587755d3594d13
almircoo <almircoo@proton.me>
2025-10-08 18:54:34 +0100

    crate category and menu items views

--- core/views.py
+++ core/views.py
@@ -16,3 +16,22 @@ class PlaceDetail(generics.RetrieveUpdateDestroyAPIView):
   permission_classes = [permissions.IsOwnerOrReadOnly]
   serializer_class = serializers.PlaceDetailSerializer
   queryset = models.Place.objects.all()
+
+class CategoryList(generics.CreateAPIView):
+  permission_classes = [permissions.PlaceOwnerOrReadOnly]
+  serializer_class = serializers.CategorySerializer
+
+class CategoryDetail(generics.UpdateAPIView, generics.DestroyAPIView):
+  permission_classes = [permissions.PlaceOwnerOrReadOnly]
+  serializer_class = serializers.CategorySerializer
+  queryset = models.Category.objects.all()
+
+class MenuItemList(generics.CreateAPIView):
+  permission_classes = [permissions.PlaceOwnerOrReadOnly]
+  serializer_class = serializers.MenuItemSerializer
+
+class MenuItemDetail(generics.UpdateAPIView, generics.DestroyAPIView):
+  permission_classes = [permissions.PlaceOwnerOrReadOnly]
+  serializer_class = serializers.MenuItemSerializer
+  queryset = models.MenuItem.objects.all()
+
